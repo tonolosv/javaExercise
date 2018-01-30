1e7ccc86f28a18530ac8a1428157b36d3d71b3d2
danu <danu@192.168.1.7>
2018-01-29 19:15:55 -0600

    Inicio

---
+++ src/numeroceroynueve/NumeroCeroyNueve.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeroceroynueve;
+
+/**
+ *
+ * @author danu
+ */
+import java.io.*;
+import java.util.*;
+public class NumeroCeroyNueve {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        char Num1;
+        System.out.print("Ingrese un número: ");
+        Num1 =(char)System.in.read();
+        
+        if(Num1>='0' && Num1<='9')
+            System.out.println(Num1 + " Es un número");
+        else
+            System.out.println(Num1 + " No es un número");
+     }
+    
+}
